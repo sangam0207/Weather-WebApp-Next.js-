be16e6eb1fb8a17bed86af1c47dc4edc3095a3c3
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2024-02-16 00:19:29 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,37 @@
+import Navbar from "@/components/Navbar";
 
 
-function Error() {
+const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error 404 Page not found
-        
-      </h1>
-    </div>
-  )
-}
+    <>
+    <div className="container-fluid main_menu">
+          <div className="row">
+            <div className="col-md-10 col-12 mx-auto">
+              <Navbar />
+            </div>
+          </div>
+        </div>
+    <div className="container-fluid main_header">
+      <div className="row">
+        <div className="col-md-10 col-12 mx-auto">
+          <div className="row">
+            <div className="col-md-6 col-12 main_header_right">
+              <figure>
+                <img src="/404.jpg" alt="error" className="img-fluid" />
+              </figure>
+            </div>
+            <div className="col-md-6 col-12 main_header_left">
+              <p>Something Went Wrong</p>
+              <h1>Error 404 , Page not found</h1>
+              <a href="/"><button>Back to Home</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></>
+   
+ 
+  );
+};
 
-export default Error
+export default ErrorPage;
